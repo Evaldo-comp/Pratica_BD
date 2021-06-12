be894260846b45cd7abfd53a2a111c9e8cba8bc4
Francisco Evaldo <65002701+Evaldo-comp@users.noreply.github.com>
2021-06-12 08:50:17 -0300

    Create chave_order_by.py

---
+++ Aula02/chave_order_by.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+from contextlib import closing
+
+dados = [
+    ["São Paulo", 43663672], ["Minas Gerais", 21292666], ["Rio de Janeiro", 17366189],["Bahia", 14930634],
+    ["Paraná", 11516840], ["Rio Grande do Sul", 11422973], ["Pernambuco", 9616621], ["Ceará", 9187103],
+    ["Pará", 8690745], ["Santa Catarina", 7252502], ["Maranhão", 7114598], ["Goiáis", 7113540],
+    ["Amazonas", 4207714], ["Espirito Santo", 4064052], ["Paraíba", 4039277], 
+    ["Rio Grande do Norte", 3534165], ["Mato Grosso", 3526220], ["Alagoas", 3351543], 
+    ["Piauí", 3281480], ["Distrito Federal", 3055149], ["Mato Grosso do Sul", 2809394],
+    ["Sergipe", 2318822], ["Rondônia", 1796460], ["Tocantins", 1590248], ["Acre", 894470],
+    ["Amapá", 861773], ["Roraima", 631181]
+] 
+
+with sqlite3.connect('Brasil.db') as conexao:
+    conexao.row_factory = sqlite3.Row
+
+    print(f"{'id':3s} {'Estado':20s} {'População':12s}")
+    print("=" * 37)
+
+    for estado in conexao.execute("select * from  estados order by populacao"):
+        print(f"{estado['id']:3d} {estado['nome']:20s} {estado['populacao']:12d}")
+   
